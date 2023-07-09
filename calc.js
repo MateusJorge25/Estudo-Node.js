0cf806c03732918d9e60c98bfab9126bb4a3c156
Mateusj2506 <mateus_jorge2002@outlook.com>
2023-07-09 11:04:08 -0300

    initial commit

---
+++ calc.js
@@ -0,0 +1,12 @@
+var somaFunc = require("./soma");
+var subFunc = require("./sub");
+var multiFunc = require("./multi");
+var divFunc = require("./div");
+
+console.log(somaFunc(4,4));
+console.log(subFunc(6,2));
+console.log(multiFunc(2,2));
+console.log(divFunc(81,9));
+
+
+
