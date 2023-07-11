444c9317e0cc5ad8cbb2ad2b84d2030a2abbddb7
Mateusj2506 <mateus_jorge2002@outlook.com>
2023-07-11 11:48:11 -0300

    criei um servidor so com node

---
+++ app.js
@@ -0,0 +1,9 @@
+/*const http = require('http');
+const port = 8081;
+                                                            //Usamos esses codigos para abrir um servidor sem usar o express so com node
+http.createServer(function(req, res){
+    res.end("Teste")
+}).listen(8081);*/
+
+
+
